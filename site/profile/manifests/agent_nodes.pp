5d7d7a9a288212deaf07d32722f1ed77715f18dc
elnazmirrahimi2018 <39279618+elnazmirrahimi2018@users.noreply.github.com>
2018-06-28 15:53:32 -0400

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -2,3 +2,7 @@ class profile::agent_nodes {
  include dockeragent
  dockeragent::node {'web.puppet.vm'}
  dockeragent::node {'db.puppet.vm'}
+ host { 'web.puppet.vm':
+   ensure => present,
+   ipaddress =>"10.0.2.15"
+   }
