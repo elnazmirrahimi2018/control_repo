cf66266c015dfb0d6c083e0ab05530719b396e08
elnazmirrahimi2018 <39279618+elnazmirrahimi2018@users.noreply.github.com>
2018-06-29 12:11:14 -0400

    Create init.pp

---
+++ site/minecraft/manifests/init.pp
@@ -0,0 +1,23 @@
+class minecraft {
+  file {'/opt/minecraft':
+    ensure => directory,
+  }
+  file {'/opt/minecraft/minecraft_server.jar:
+    ensure => file,
+    source =>'https://s3.amazonaws.com',
+   }
+   package {'java':
+     ensure => present,
+   }
+   file { '/opt/minecraft/eula.txt':
+     ensure => file,
+     content =>'eula=true',
+   }
+   file {'/etc/systemd/system/minecraft.service':
+      ensure => file,
+      source => 'puppet:///modules/minecraft/minecraft.service',
+   }
+   
+ }
+   
+ }
